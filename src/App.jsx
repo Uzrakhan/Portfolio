1cebb6a5d93319ebdd0dd1d0356006ab803914a0
Uzrakhan <uzrakhan539@gmail.com>
2025-02-21 20:16:20 +0530

    Routing issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import {HashRouter as Router, Route, Routes} from 'react-router-dom';
 import NavBar from './components/NavBar';
 import Footer from './components/Footer';
 import Home from './pages/Home';
